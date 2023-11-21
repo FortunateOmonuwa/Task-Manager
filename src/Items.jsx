079106115122365130f9d7ccc48059f0ba5b9371
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2023-11-21 12:24:42 +0100

    add checked

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Items = ({ todoItem, completed }) => {
+  const [checked, setCheck] = useState(completed);
+
+  function handleCheck() {
+    setCheck((prev) => !prev);
+  }
   return (
-    <div className="items">
-      <input type="checkbox" name="" id="" />
+    <div className={checked ? "checked" : "items"}>
+      <input
+        type="checkbox"
+        name=""
+        id=""
+        checked={checked}
+        onClick={handleCheck}
+      />
       <label htmlFor="checkbox">{todoItem}</label>
       <button type="danger">Delete</button>
     </div>
